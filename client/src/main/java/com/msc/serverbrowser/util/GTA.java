cda8de3b644e82099a5f1c0d1f90b91078bfb6c3
Oliver Jan Krylow <oliver@bugabinga.net>
2017-06-27 01:32:16 +0200

    Initial support for javapacker builds. Native builds tested on Fedora. client can already be run with __./gradlew client:jxfRun__. server needs some params and mysql...

--- client/src/main/java/com/msc/serverbrowser/util/GTA.java
+++ client/src/main/java/com/msc/serverbrowser/util/GTA.java
@@ -23,54 +23,38 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Modality;
 
-public class GTA
-{
-	private static StringProperty username = new SimpleStringProperty(retrieveUsernameFromRegistry());
-
-	public static StringProperty usernameProperty()
-	{
-		return username;
-	}
+public class GTA {
+	public static StringProperty usernameProperty = new SimpleStringProperty(retrieveUsernameFromRegistry());
 
 	/**
-	 * Writes the actual username (from registry) into the past usernames list and sets the new name
+	 * Writes the actual username (from registry) into the past usernames list
+	 * and sets the new name
 	 */
-	public static void applyUsername()
-	{
+	public static void applyUsername() {
 		killSamp();
 		PastUsernames.addPastUsername(retrieveUsernameFromRegistry());
-		try
-		{
-			WindowsRegistry.getInstance().writeStringValue(HKey.HKCU, "SOFTWARE\\SAMP", "PlayerName", usernameProperty().get());
-		}
-		catch (final RegistryException e)
-		{
+		try {
+			WindowsRegistry.getInstance().writeStringValue(HKey.HKCU, "SOFTWARE\\SAMP", "PlayerName",
+					usernameProperty().get());
+		} catch (final RegistryException e) {
 			Logging.logger.log(Level.WARNING, "Couldn't set username.", e);
 		}
 
 	}
 
-	private static String retrieveUsernameFromRegistry()
-	{
-		try
-		{
+	private static String retrieveUsernameFromRegistry() {
+		try {
 			return WindowsRegistry.getInstance().readString(HKey.HKCU, "SOFTWARE\\SAMP", "PlayerName");
-		}
-		catch (final Exception exception)
-		{
+		} catch (final Exception exception) {
 			Logging.logger.log(Level.WARNING, "Couldn't retrieve Username from registry.", exception);
 			return "404 Name not found";
 		}
 	}
 
-	// Can't be null, Eclipse just doesn't acknowledge the Objects.nonNull method as a valid
-	// nullcheck
-	public static Optional<String> getGtaPath()
-	{
+	public static Optional<String> getGtaPath() {
 		String property = ClientProperties.getPropertyAsString(Property.SAMP_PATH);
 		property = Objects.isNull(property) || property.isEmpty() ? null : property;
-		if (Objects.nonNull(property) && !property.endsWith("\\"))
-		{
+		if (Objects.nonNull(property) && !property.endsWith("\\")) {
 			property = property + "\\";
 		}
 
@@ -82,71 +66,61 @@ public class GTA
 	 *
 	 * @return String of the GTA Path or null.
 	 */
-	public static String getGtaPathUnsafe()
-	{
-		try
-		{
-			return WindowsRegistry.getInstance().readString(HKey.HKCU, "SOFTWARE\\SAMP", "gta_sa_exe").replace("gta_sa.exe", "");
-		}
-		catch (final RegistryException exception)
-		{
+	public static String getGtaPathUnsafe() {
+		try {
+			return WindowsRegistry.getInstance().readString(HKey.HKCU, "SOFTWARE\\SAMP", "gta_sa_exe")
+					.replace("gta_sa.exe", "");
+		} catch (final RegistryException exception) {
 			Logging.logger.log(Level.WARNING, "Couldn't retrieve GTA path.", exception);
 			return null;
 		}
 	}
 
-	public static Optional<String> getInstalledVersion()
-	{
+	public static Optional<String> getInstalledVersion() {
 		String versionString = null;
 		final Optional<String> path = getGtaPath();
-		if (path.isPresent())
-		{
+		if (path.isPresent()) {
 			final File file = new File(path.get() + "samp.dll");
 
 			/*
-			 * Bad Practice, could potentionally cause an error if Kalcor decides to do a huge
-			 * update someday :P
+			 * Bad Practice, could potentionally cause an error if Kalcor
+			 * decides to do a huge update someday :P
 			 */
-			switch ((int) file.length())
-			{
-				case 2199552:
-					versionString = "0.3.7";
-					break;
-				case 1093632:
-					versionString = "0.3z";
-					break;
-				case 2084864:
-					versionString = "0.3x";
-					break;
-				case 1998848:
-					versionString = "0.3e";
-					break;
-				case 2015232:
-					versionString = "0.3d";
-					break;
-				case 1511424:
-					versionString = "0.3c";
-					break;
-				case 610304:
-					versionString = "0.3a";
-					break;
+			switch ((int) file.length()) {
+			case 2199552:
+				versionString = "0.3.7";
+				break;
+			case 1093632:
+				versionString = "0.3z";
+				break;
+			case 2084864:
+				versionString = "0.3x";
+				break;
+			case 1998848:
+				versionString = "0.3e";
+				break;
+			case 2015232:
+				versionString = "0.3d";
+				break;
+			case 1511424:
+				versionString = "0.3c";
+				break;
+			case 610304:
+				versionString = "0.3a";
+				break;
 			}
 		}
 
 		return Optional.ofNullable(versionString);
 	}
 
-	private static boolean connectToServerUsingProtocol(final String ipAndPort)
-	{
+	private static boolean connectToServerUsingProtocol(final String ipAndPort) {
 		Logging.logger.log(Level.INFO, "Connecting using protocol.");
-		try
-		{
+		try {
 			final Desktop d = Desktop.getDesktop();
 			d.browse(new URI("samp://" + ipAndPort));
 			return true;
-		}
-		catch (final Exception exception)
-		{
+		} catch (final Exception exception) {
 			exception.printStackTrace();
 			return false;
 		}
@@ -155,58 +129,43 @@ public class GTA
 	/**
 	 * Kills SA-MP using the command line.
 	 */
-	public static void killSamp()
-	{
+	public static void killSamp() {
 		kill("samp.exe");
 	}
 
 	/**
 	 * Kills GTA using the command line.
 	 */
-	public static void killGTA()
-	{
+	public static void killGTA() {
 		kill("gta_sa.exe");
 	}
 
-	private static void kill(final String application)
-	{
-		try
-		{
+	private static void kill(final String application) {
+		try {
 			Runtime.getRuntime().exec("taskkill /F /IM " + application);
-		}
-		catch (final IOException exception)
-		{
+		} catch (final IOException exception) {
 			Logging.logger.log(Level.SEVERE, "Couldn't kill " + application, exception);
 		}
 	}
 
-	public static void connectToServer(final String ipAndPort, final String password)
-	{
+	public static void connectToServer(final String ipAndPort, final String password) {
 		killGTA();
 		final Optional<String> gtaPath = getGtaPath();
-		if (gtaPath.isPresent())
-		{
-			try
-			{
+		if (gtaPath.isPresent()) {
+			try {
 				Logging.logger.log(Level.INFO, "Connecting using executeable.");
-				final ProcessBuilder builder = new ProcessBuilder(gtaPath.get() + File.separator + "samp.exe ", ipAndPort, password);
+				final ProcessBuilder builder = new ProcessBuilder(gtaPath.get() + File.separator + "samp.exe ",
+						ipAndPort, password);
 				builder.directory(new File(gtaPath.get()));
 				builder.start();
-			}
-			catch (final Exception exception)
-			{
-				if (Objects.isNull(password) || password.isEmpty())
-				{
+			} catch (final Exception exception) {
+				if (Objects.isNull(password) || password.isEmpty()) {
 					connectToServerUsingProtocol(ipAndPort);
-				}
-				else
-				{
+				} else {
 					Logging.logger.log(Level.WARNING, "Couldn't connect to server", exception);
 				}
 			}
-		}
-		else
-		{
+		} else {
 			final Alert alert = new Alert(AlertType.ERROR);
 			Client.setAlertIcon(alert);
 			alert.initOwner(Client.getInstance().getStage());
@@ -218,8 +177,7 @@ public class GTA
 		}
 	}
 
-	public static void connectToServer(final String ipAndPort)
-	{
+	public static void connectToServer(final String ipAndPort) {
 		connectToServer(ipAndPort, "");
 	}
 }
