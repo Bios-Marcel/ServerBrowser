4b6c604cc46118d73eef0aee0251ea00b98e49f1
[Bios]Marcel <marceloschr@googlemail.com>
2017-07-08 11:21:16 +0200

    Fix stylesheet path

--- client/src/main/java/com/msc/serverbrowser/constants/Paths.java
+++ client/src/main/java/com/msc/serverbrowser/constants/Paths.java
@@ -7,5 +7,5 @@ public class Paths
 	public static final String	USER_PATH		= System.getProperty("user.home") + File.separator;
 	public static final String	SAMPEX_PATH		= USER_PATH + "sampex";
 	public static final String	VIEW_PATH		= "/com/msc/serverbrowser/views/";
-	public static final String	STYLESHEET_PATH	= VIEW_PATH + "styles";
+	public static final String	STYLESHEET_PATH	= VIEW_PATH + "stylesheets/";
 }
