237354771ac9a1748c09e30cff23ffba0c9c3a49
[Bios]Marcel <marceloschr@googlemail.com>
2017-09-23 23:06:00 +0200

    Simplify and document compareVersions

--- client/src/main/java/com/msc/serverbrowser/util/UpdateUtility.java
+++ client/src/main/java/com/msc/serverbrowser/util/UpdateUtility.java
@@ -115,42 +115,33 @@ public final class UpdateUtility
 	 */
 	public static CompareResult compareVersions(final String versionOne, final String versionTwo) throws NullPointerException, NumberFormatException, IllegalArgumentException
 	{
+		// Throw NullPointer if any is null and throw IllegalArgument if any is empty
 		final String trimmedOne = versionOne.trim();
 		final String trimmedTwo = versionTwo.trim();
-
 		if (trimmedOne.isEmpty() || trimmedTwo.isEmpty())
 		{
 			throw new IllegalArgumentException("Empty versionstrings are invalid. (One: '" + trimmedOne + "' Two: '" + trimmedTwo + "')");
 		}
 
+		// Split Versions into their subversions;
 		final String[] versionOneParts = trimmedOne.split("[.]");
 		final String[] versionTwoParts = trimmedTwo.split("[.]");
 
+		// Check if the Version matches the maximum number of subversions
 		final int longest = Integer.max(versionOneParts.length, versionTwoParts.length);
-
 		if (longest > 3)
 		{
 			throw new IllegalArgumentException("The semantic version should only have up to 3 parts, eg: '1.23.2312'.");
 		}
 
+		// Do first comparison
 		final int length = Integer.min(versionOneParts.length, versionTwoParts.length);
+		final Optional<CompareResult> result = compareVersionsUpToIndex(versionOneParts, versionTwoParts, length);
 
-		for (int index = 0; index < length; index++)
-		{
-			final Integer numberOne = Integer.parseInt(versionOneParts[index]);
-			final Integer numberTwo = Integer.parseInt(versionTwoParts[index]);
-
-			if (numberOne < numberTwo)
-			{
-				return CompareResult.LESS;
-			}
-			if (numberTwo < numberOne)
-			{
-				return CompareResult.GREATER;
-			}
-		}
-
-		if (longest > length)
+		// Incase we don't have any result as of now, we check furhter
+		final boolean arrayNotEquallyLong = longest > length;
+		final boolean noBiggerOneFoundYet = !result.isPresent();
+		if (noBiggerOneFoundYet && arrayNotEquallyLong)
 		{// If one of the strings had more version parts, we will check those too.
 			final String[] bigger = ArrayUtility.getLonger(versionOneParts, versionTwoParts);
 			final boolean foundOneBiggerThanZero = IntStream.range(length, longest)
@@ -167,6 +158,26 @@ public final class UpdateUtility
 			}
 		}
 
-		return CompareResult.EQUAL;
+		return result.orElse(CompareResult.EQUAL);
+	}
+
+	private static Optional<CompareResult> compareVersionsUpToIndex(final String[] versionOneParts, final String[] versionTwoParts, final int length)
+	{
+		for (int index = 0; index < length; index++)
+		{
+			final Integer numberOne = Integer.parseInt(versionOneParts[index]);
+			final Integer numberTwo = Integer.parseInt(versionTwoParts[index]);
+
+			if (numberOne < numberTwo)
+			{
+				return Optional.of(CompareResult.LESS);
+			}
+			if (numberTwo < numberOne)
+			{
+				return Optional.of(CompareResult.GREATER);
+			}
+		}
+
+		return Optional.empty();
 	}
 }
