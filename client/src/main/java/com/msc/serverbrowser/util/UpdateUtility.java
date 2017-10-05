2193434e4900b43c984dc1fe50677b2059e4fe3e
[Bios]Marcel <marceloschr@googlemail.com>
2017-10-05 19:13:33 +0200

    No more search and replace all

--- client/src/main/java/com/msc/serverbrowser/util/UpdateUtility.java
+++ client/src/main/java/com/msc/serverbrowser/util/UpdateUtility.java
@@ -24,7 +24,7 @@ public final class UpdateUtility
 	/**
 	 * The current version of this application
 	 */
-	public static final String VERSION = "1.4.3";
+	public static final String VERSION = "1.4.4";
 
 	private UpdateUtility()
 	{
