68f0da433f2bc1a44869e0b453313fc2d39a1e75
[Bios]Marcel <marceloschr@googlemail.com>
2018-01-10 06:40:35 +0100

    # Improve Player TableModel

--- client/src/main/java/com/msc/serverbrowser/data/entites/Player.java
+++ client/src/main/java/com/msc/serverbrowser/data/entites/Player.java
@@ -1,27 +1,50 @@
 package com.msc.serverbrowser.data.entites;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
-@SuppressWarnings("javadoc")
+/**
+ * <p>
+ * Tablemodel for the Players of a server, only holds two properties:
+ * </p>
+ * <ul>
+ * <li>Name</li>
+ * <li>Score</li>
+ * </ul>
+ *
+ * @author marcel
+ * @since Jan 10, 2018
+ */
 public class Player {
+	/** Property for holding the players name */
 	private final StringProperty	playerName	= new SimpleStringProperty();
-	private final StringProperty	playerScore	= new SimpleStringProperty();
+	/** Property for holding the players score */
+	private final IntegerProperty	playerScore	= new SimpleIntegerProperty();
 	
-	public Player(final String playerName, final String playerScore) {
+	/**
+	 * @param playerName
+	 *            Ingame name of the player
+	 * @param playerScore
+	 *            Ingame score of the player
+	 */
+	public Player(final String playerName, final Integer playerScore) {
 		this.playerName.set(playerName);
 		this.playerScore.set(playerScore);
 	}
 	
-	public Player(final String playerName, final Integer playerScore) {
-		this(playerName, playerScore.toString());
-	}
-	
+	/**
+	 * @return {@link #playerName}
+	 */
 	public StringProperty playerNameProperty() {
 		return playerName;
 	}
 	
-	public StringProperty playerScoreProperty() {
+	/**
+	 * @return {@link #playerScore}
+	 */
+	public IntegerProperty playerScoreProperty() {
 		return playerScore;
 	}
 }
