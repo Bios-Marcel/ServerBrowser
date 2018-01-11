20cbc65f56a9a0da8b572eb39e2a8b7da747b27b
[Bios]Marcel <marceloschr@googlemail.com>
2018-01-11 19:01:04 +0100

    Update to next release ready version

--- client/src/main/java/com/msc/serverbrowser/util/UpdateUtility.java
+++ client/src/main/java/com/msc/serverbrowser/util/UpdateUtility.java
@@ -21,14 +21,25 @@ import com.msc.serverbrowser.util.basic.ArrayUtility;
  */
 public final class UpdateUtility {
 	/**
-	 * The current version of this application
+	 * <p>
+	 * The current version of this application.
+	 * </p>
+	 * <p>
+	 * For the record: the old version scheme was X.X.X, increasing the first only when gigantic
+	 * changes where made, the second when new features where added and such and the last one when
+	 * minor changes to the ui, language or whatever where made.
+	 * </p>
+	 * <p>
+	 * In the new version scheme i will include the major version of the compatible jre version,
+	 * since in the feature i'll to know this as for having to update the jre aswell.
+	 * </p>
 	 */
-	public static final String VERSION = "1.4.4";
-	
+	public static final String VERSION = "8.1.5.0";
+
 	private UpdateUtility() {
 		// Constructor to prevent instantiation
 	}
-	
+
 	/**
 	 * Checks if the currently installed version is the latest.
 	 *
@@ -38,11 +49,11 @@ public final class UpdateUtility {
 	 */
 	public static Boolean isUpToDate() throws IOException {
 		final String latestVersion = getRelease().get().getTagName();
-		
+
 		final CompareResult result = compareVersions(VERSION, latestVersion);
 		return result != CompareResult.LESS;
 	}
-	
+
 	/**
 	 * @return a {@link GHRelease} for the latest ServerBrowser release
 	 * @throws IOException
@@ -57,7 +68,7 @@ public final class UpdateUtility {
 		}
 		return Optional.empty();
 	}
-	
+
 	/**
 	 * Compares two version strings to each other.
 	 *
@@ -75,35 +86,37 @@ public final class UpdateUtility {
 	 * @throws IllegalArgumentException
 	 *             if any of the arguments is empty
 	 */
-	public static CompareResult compareVersions(final String versionOne, final String versionTwo) throws NullPointerException, NumberFormatException, IllegalArgumentException {
+	public static CompareResult compareVersions(final String versionOne, final String versionTwo)
+			throws NullPointerException, NumberFormatException, IllegalArgumentException {
 		// Throw NullPointer if any is null and throw IllegalArgument if any is empty
 		final String trimmedOne = versionOne.trim();
 		final String trimmedTwo = versionTwo.trim();
 		if (trimmedOne.isEmpty() || trimmedTwo.isEmpty()) {
 			throw new IllegalArgumentException("Empty versionstrings are invalid. (One: '" + trimmedOne + "' Two: '" + trimmedTwo + "')");
 		}
-		
+
 		// Split Versions into their subversions;
 		final String[] versionOneParts = trimmedOne.split("[.]");
 		final String[] versionTwoParts = trimmedTwo.split("[.]");
-		
+
 		// Check if the Version matches the maximum number of subversions
 		final int longest = Integer.max(versionOneParts.length, versionTwoParts.length);
 		if (longest > 3) {
 			throw new IllegalArgumentException("The semantic version should only have up to 3 parts, eg: '1.23.2312'.");
 		}
-		
+
 		// Do first comparison
 		final int length = Integer.min(versionOneParts.length, versionTwoParts.length);
 		final Optional<CompareResult> result = compareVersionsUpToIndex(versionOneParts, versionTwoParts, length);
-		
+
 		// In case we don't have any result as of now, we check further
 		final boolean arrayNotEquallyLong = longest > length;
 		final boolean noBiggerOneFoundYet = !result.isPresent();
-		if (noBiggerOneFoundYet && arrayNotEquallyLong) {// If one of the strings had more version parts, we will check those too.
+		if (noBiggerOneFoundYet && arrayNotEquallyLong) {// If one of the strings had more version
+															// parts, we will check those too.
 			final String[] bigger = ArrayUtility.getLonger(versionOneParts, versionTwoParts).get();
 			final boolean foundOneBiggerThanZero = IntStream.range(length, longest).filter(num -> Integer.parseInt(bigger[num]) != 0).findAny().isPresent();
-			
+
 			if (foundOneBiggerThanZero) {
 				if (bigger == versionOneParts) {
 					return CompareResult.GREATER;
@@ -111,15 +124,15 @@ public final class UpdateUtility {
 				return CompareResult.LESS;
 			}
 		}
-		
+
 		return result.orElse(CompareResult.EQUAL);
 	}
-	
+
 	private static Optional<CompareResult> compareVersionsUpToIndex(final String[] versionOneParts, final String[] versionTwoParts, final int length) {
 		for (int index = 0; index < length; index++) {
 			final Integer numberOne = Integer.parseInt(versionOneParts[index]);
 			final Integer numberTwo = Integer.parseInt(versionTwoParts[index]);
-			
+
 			if (numberOne < numberTwo) {
 				return Optional.of(CompareResult.LESS);
 			}
@@ -127,7 +140,7 @@ public final class UpdateUtility {
 				return Optional.of(CompareResult.GREATER);
 			}
 		}
-		
+
 		return Optional.empty();
 	}
 }
