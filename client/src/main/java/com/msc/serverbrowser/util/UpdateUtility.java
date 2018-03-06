ee4319d70d8fe1a47bb27f213e71c5eab4fa0528
[Bios]Marcel <marceloschr@googlemail.com>
2018-03-06 23:23:23 +0100

    # Increase Version number

--- client/src/main/java/com/msc/serverbrowser/util/UpdateUtility.java
+++ client/src/main/java/com/msc/serverbrowser/util/UpdateUtility.java
@@ -36,7 +36,7 @@ public final class UpdateUtility {
 	 * since in the feature i'll to know this as for having to update the jre aswell.
 	 * </p>
 	 */
-	public static final String VERSION = "8.5.6";
+	public static final String VERSION = "8.5.7";
 
 	private UpdateUtility() {
 		// Constructor to prevent instantiation
